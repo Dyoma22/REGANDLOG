bc44bf11a97add52d094ae7183758db296627cf1
Дёма <@WAKEUPDYOMA>
2021-11-18 09:48:24 +0200

    Добавьте файлы проекта.

---
+++ PythonApplication2/PythonApplication2.py
@@ -0,0 +1,19 @@
+﻿from Funktsioonid import *
+users=["Demid"]
+passwords=["12345"]
+
+while True:
+    print("Reg-1,Avt-2,Väljä-3")
+    v=int(input())
+    if v==1:
+        print("Registreerimine")
+        #
+    elif v==2:
+        print("Avtoriseerimine")
+        #
+    elif v==3:
+        print("Väljä")
+        break
+        #valmis
+    else:
+        print("On vaja valida 1,2 või 3")
