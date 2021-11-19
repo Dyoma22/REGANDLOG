674c576d04b61dc3b96c9909e26dbd05ff24e336
Дёма <@WAKEUPDYOMA>
2021-11-19 11:05:53 +0200

    xdd

--- PythonApplication2/Funktsioonid.py
+++ PythonApplication2/Funktsioonid.py
@@ -1,9 +1,9 @@
 import random
 #Cоздаем функции
 def passautomat()->str:
-	"""
+	"""Пароль создается машиной
 	"""	
-	str0=".,_"
+	str0=".,:;!_*-+()/#¤%&"
 	str1 = '0123456789'
 	str2 = 'qwertyuiopasdfghjklzxcvbnm'
 	str3 = str2.upper() # 'QWERTYUIOPASDFGHJKLZXCVBNM'
@@ -14,3 +14,16 @@ def passautomat()->str:
 	psword = ''.join([random.choice(ls) for x in range(12)])
 # Пароль готов
 	return psword
+def paskontroll(psword: str)->bool:
+	ls=list(psword)
+	for e in ls:
+		if e.isdigit(): d=True
+		if e.isalpha(): a=True
+		if e.isupper(): u=True
+		if e.islower():l=True
+		if e in list(".,:;!_*-+()/#¤%&"): s=True
+	if d==True and a==True and u==True and l==True and s==True:
+		t=True
+	else:
+		t=False
+	return t
--- PythonApplication2/PythonApplication2.py
+++ PythonApplication2/PythonApplication2.py
@@ -3,21 +3,36 @@ users=["Demid"]
 passwords=["12345"]
 
 while True:
-    print("Reg-1,Avt-2,Väljä-3")
-    v=int(input())
-    if v==1:
-        print("Registreerimine")
-        #
-        #log=...
-        #pas=...
-        users.append(log)
-        passwords.append(pas)
-    elif v==2:
-        print("Avtoriseerimine")
-        #
-    elif v==3:
-        print("Väljä")
-        break
-        #valmis
-    else:
-        print("On vaja valida 1,2 või 3")
+	print("Reg-1,Avt-2,Välja-3")
+	v=int(input())
+	if v==1:
+		print("Registreerimine")
+		while 1:
+			log=input("Kasutajatunnus")
+			if log not in users:
+				print("Tunnus soobib")
+				break
+			else:
+				print("See nimi juba on olemas listis")	
+		v=input("Arvuti-A või ise-I loob parool")
+		if v.upper()=="A":
+			pas=passautomat()
+		elif v.upper()=="I":			
+			while 1:
+				pas=input("Sisesta oma parool")
+				tulemus=(pas)
+				if tulemus==True:
+					users.append(log)
+					passwords.append(pas)
+					break
+
+		
+	elif v==2:
+		print("Avtoriseerimine")
+		#
+	elif v==3:
+		print("Välja")
+		break
+		#valmis
+	else:
+		print("On vaja valida 1,2 või 3")# kõik on olemas
